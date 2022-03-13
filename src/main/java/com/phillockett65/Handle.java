45e73ed3453d2bfe37b8995ee0fac75bcc6b780a
Phil Lockett <phillockett65@gmail.com>
2022-03-13 12:56:52 -0400

    Updated copyright notice.

--- src/main/java/com/phillockett65/Handle.java
+++ src/main/java/com/phillockett65/Handle.java
@@ -1,21 +1,21 @@
-/*  CardGen - a playing card image generator.
+/*  CardCreate - a playing card image generator.
  *
- *  Copyright 2020 Philip Lockett.
+ *  Copyright 2022 Philip Lockett.
  *
- *  This file is part of CardGen.
+ *  This file is part of CardCreate.
  *
- *  CardGen is free software: you can redistribute it and/or modify
+ *  CardCreate is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  CardGen is distributed in the hope that it will be useful,
+ *  CardCreate is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with CardGen.  If not, see <https://www.gnu.org/licenses/>.
+ *  along with CardCreate.  If not, see <https://www.gnu.org/licenses/>.
  */
 package com.phillockett65;
 
